e62639d06ec578db86639f4311b36631392a0b5c
rajat-vig <rajatvig71@gmail.com>
2017-11-12 02:17:47 -0500

    Add friendship and friend search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
-   get 'my_portfolio', to: 'users#my_portfolio'
-   get 'search_stocks', to: 'stocks#search'
-   get 'my_friends', to: 'users#my_friends'
-  
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
